639ef7a9a7f609ceb3f4621e007dec8ce51c3492
ronak1790 <ronak1790@gmail.com>
2019-06-03 17:45:31 -0500

    Initial check-in of module LeetcodeProblems

---
+++ LeetcodeProblems/Problem1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace LeetcodeProblems {
+    public class Problem1 {
+        public Problem1() {
+
+        }
+    }
+}
---
+++ LeetcodeProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LeetcodeProblems {
+    class MainClass {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
